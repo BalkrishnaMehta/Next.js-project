c74a3dc80a9e8a7a30813379bbbbd491e1c18f89
Balkrishna Mehta <95116756+Krish-208@users.noreply.github.com>
2023-07-02 02:27:09 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import '@/styles/globals.css';
 import Sidebar from '../components/Sidebar';
-import Header from '../components/Header'
+import Header from '../components/Header';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, router }) {
+  const excludeComponents = ['/login']; 
+
+  const shouldExcludeComponents = excludeComponents.includes(router.pathname);
+
   return (
     <>
-      <Header />
-      <Sidebar>
-        <Component {...pageProps} />
-      </Sidebar>
+      {!shouldExcludeComponents && <Header />}
+      {!shouldExcludeComponents && (
+        <Sidebar>
+          <Component {...pageProps} />
+        </Sidebar>
+      )}
+      {shouldExcludeComponents && <Component {...pageProps} />}
     </>
   );
 }
